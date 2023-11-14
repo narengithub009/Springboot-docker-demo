016a8d294b517d02b8514901a09d571386efa23f
narengithub009 <narender.kon09@gmail.com>
2023-11-14 09:55:08 +0530

    first commit 3

--- src/main/java/com/java/demo/SpringbootDockerDemoApplication.java
+++ src/main/java/com/java/demo/SpringbootDockerDemoApplication.java
@@ -2,6 +2,8 @@ package com.java.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api")
@@ -11,7 +13,7 @@ public class SpringbootDockerDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerDemoApplication.class, args);
 	}
-	
+	@GetMapping("/msg")
 	public String getMessage() {
 		return "Hello docker jenkinc CICD integration.";
 	}
