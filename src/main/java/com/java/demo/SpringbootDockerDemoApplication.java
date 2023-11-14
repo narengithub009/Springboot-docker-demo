3cb59bbedd76c2f570a665831df0a283c984813b
narengithub009 <narender.kon09@gmail.com>
2023-11-14 08:42:55 +0530

    added api request..

--- src/main/java/com/java/demo/SpringbootDockerDemoApplication.java
+++ src/main/java/com/java/demo/SpringbootDockerDemoApplication.java
@@ -2,12 +2,17 @@ package com.java.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController("/api")
 @SpringBootApplication
 public class SpringbootDockerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerDemoApplication.class, args);
 	}
-
+	
+	public String getMessage() {
+		return "Hello docker jenkinc CICD integration.";
+	}
 }
